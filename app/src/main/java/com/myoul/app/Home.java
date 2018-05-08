fefd4f4dbccee71d8047ebc6c792fa4c40a36ab1
Nick Mounier <mounien@wwu.edu>
2018-05-08 16:37:59 -0700

    working on login logic added home page

---
+++ app/src/main/java/com/myoul/app/Home.java
@@ -0,0 +1,22 @@
+package com.myoul.app;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(Login.LEAD);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
+    }
+}
