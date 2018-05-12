2e133d8af18085ed5e9c04c3a17783798d1ee50b
Nick Mounier <mounien@wwu.edu>
2018-05-11 22:03:47 -0700

    started server work

--- Myoul DB/src/main/java/com/example/myouldb/MyoulDB.java
+++ Myoul DB/src/main/java/com/example/myouldb/MyoulDB.java
@@ -1,4 +1,24 @@
 package com.example.myouldb;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class MyoulDB {
+
+    private static final int port = 3666;
+    private static ServerSocket server;
+
+    public static void main(String[] args){
+
+        try {
+            server = new ServerSocket(port);
+            while(true){
+                Socket sock = server.accept();
+                //create a new thread to handle the socket
+            }
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
 }
